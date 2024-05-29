033017e6576fc862117b0ebbc299c9dce989cfc3
BRADDTELES <braddteles@gmail.com>
2024-05-29 16:34:10 -0300

    variaveis e concatenação

--- aula02variaveis/index.php
+++ aula02variaveis/index.php
@@ -3,18 +3,19 @@
 $nome = "Danillo";
 $idade = 34;
 
-$html = "<!DOCTYPE html>
+$head = "<!DOCTYPE html>
 <html lang='pt'>
 <head>
 
     <title>Site de PHP by <?= $nome ?></title>
-</head>
+</head>";
+$body = "
 <body>
-    <h1>Meu nome é $nome, tenho $idade anos.</h1>
+    <h1>Meu nome é $nome, tenho $idade anos. Novamente</h1>
 </body>
 </html>
 ";
 
+$html = $head . $body;
 echo $html;
 
-echo "<br><br>";
